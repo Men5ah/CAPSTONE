75af3d809be085fd877c39d054aa358d1c394afe
Men5ah <96393336+Men5ah@users.noreply.github.com>
2025-05-21 12:23:03 +0000

    Final Commit

--- Deployment/test.py
+++
@@ -1,6 +0,0 @@
-import joblib
-
-# Load feature info to check its structure
-feature_info_path = 'C:/xampp/htdocs/Projects/CAPSTONE/Deployment/feature_info_noisy.pkl'
-feature_info = joblib.load(feature_info_path)
-print(feature_info)  # Check the structure of the loaded feature info
